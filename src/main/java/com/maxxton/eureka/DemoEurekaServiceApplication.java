5c354eda0a42608814d22d80c20142caa624312a
Mick Burgs <burgs.m@Maxxtonbv.middelburg>
2016-03-09 15:51:31 +0100

    init

---
+++ src/main/java/com/maxxton/eureka/DemoEurekaServiceApplication.java
@@ -0,0 +1,14 @@
+package com.maxxton.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class DemoEurekaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoEurekaServiceApplication.class, args);
+	}
+}
